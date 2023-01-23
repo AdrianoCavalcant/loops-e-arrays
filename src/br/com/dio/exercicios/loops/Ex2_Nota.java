26ce13ef171eb980a2bc515aaf2f9a15868939b6
AdrianoCavalcant <silvaadriano233@gmail.com>
2023-01-23 18:11:10 -0300

    loops e arrays

--- src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -11,6 +11,7 @@ até que o usuário informe um valor válido.
 public class Ex2_Nota {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
 
         int nota;
 
@@ -21,6 +22,7 @@ public class Ex2_Nota {
             System.out.println("Nota Inválida! Digite novamente: ");
             nota = scan.nextInt();
         }
+        
 
     }
 
